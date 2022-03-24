543409b65f6da0878e6462a97e29a0f2c7e6921a
Mohd Mazhar Uddin <76607351+mazhar343@users.noreply.github.com>
2022-03-24 16:28:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-$(document).ready(function () {
-    let cellContainer = $(".input-cell-container");
+for (let i = 1; i <= 100; i++) {
+    let ans = "";
 
-    for (let i = 1; i < 100; 1++) {
-        let ans = "";
-        let n = 1;
-        while (n > 0) {
-            let rem = n % 26;
-            if (rem == 0) {
-                ans = 'Z' + ans;
-            }
-            else {
-                ans = String.fromCharCode(rem - 1 + 65) + ans;
-                n = Math.floor(n / 26);
-            }
+    let n = i;
+
+    while (n > 0) {
+        let rem = n % 26;
+        if (rem == 0) {
+            ans = "Z" + ans;
+            n = Math.floor(n / 26) - 1;
+        } else {
+            ans = String.fromCharCode(rem - 1 + 65) + ans;
+            n = Math.floor(n / 26);
         }
-        let column =$('<div class="column-name" id="colCod-${ans} colId-${i}">${}</div>');
-        $(".column-name-cotainer").append(column);
     }
-});
+
+    let column = $(`<div class="column-name colId-${i}" id="colCod-${ans}">${ans}</div>`);
+    $(".column-name-container").append(column);
+    let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+    $(".row-name-container").append(row);
+
+    
+
+}
+
+for(let i=1; i<=100;i++){
+    let row=$(`<div class="cell-row"></div>`);
+    for(let j=1;j<=100;j++){
+        let colCode=$(`.colId-${j}`).attr("id").split("-")[1]
+        let column=$(`<div class="input-cell " contenteditable="true" id="row-${i}-col${j}" data="code-${colCode}" ></div>`)
+        row.append(column);
+    }
+    $(".input-cell-container").append(row);
+}
